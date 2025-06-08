84ddf8fe56b2f24a97f79fb734a03fe49bdfb2d5
yatharthrathii <yatharthmaheshwari01@gmail.com>
2025-06-08 20:09:52 +0530

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
-import About from './pages/About';
-import Store from './pages/Store';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import ProductDetail from './pages/ProductDetail';
-import LoginForm from './pages/LoginForm'; 
-import SignupForm from './pages/SignupForm';
+
+const About = lazy(() => import('./pages/About'));
+const Store = lazy(() => import('./pages/Store'));
+const Home = lazy(() => import('./pages/Home'));
+const Contact = lazy(() => import('./pages/Contact'));
+const ProductDetail = lazy(() => import('./pages/ProductDetail'));
+const LoginForm = lazy(() => import('./pages/LoginForm'));
+const SignupForm = lazy(() => import('./pages/SignupForm'));
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <HeroSection />
-              <Home />
-            </>
-          }
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignupForm />} />
-      </Routes>
+      <Suspense fallback={<div className="text-center mt-10">Loading...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <HeroSection />
+                <Home />
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
